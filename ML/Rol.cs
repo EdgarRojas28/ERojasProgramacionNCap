3b6f5fac9b51534619174de3d339841c81205ed2
EdgarRojas28 <edgarrojasvaldes0536@gmail.com>
2025-08-06 15:12:19 -0600

    Validaciones y formulario

--- ML/Rol.cs
+++ ML/Rol.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace ML
 {
     public class Rol
     {
+        [Required(ErrorMessage = "Selecciona un rol válido.")]
         public int IdRol { get; set; }
         public string? Nombre { get; set; }
         public List<object>? Roles { get; set; }
