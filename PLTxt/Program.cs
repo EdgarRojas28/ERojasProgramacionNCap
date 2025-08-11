e1abaa6465b897f158c61d3242811d442e759ad7
EdgarRojas28 <edgarrojasvaldes0536@gmail.com>
2025-08-11 13:17:14 -0600

    Filtro de Texto 1

---
+++ PLTxt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //string filePath = "C:\\Users\\digis\\OneDrive\\Documents\\Rojas Valdes Edgar Alejandro\\Rojas Valdes Edgar Alejandro\\TXT\\Txt1.txt";
+        string filePath = "C:\\Users\\digis\\OneDrive\\Documents\\Rojas Valdes Edgar Alejandro\\Rojas Valdes Edgar Alejandro\\TXT\\Txt2.txt";
+        try
+        {
+            
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string textToSplit in lines) 
+            {                
+                string[] parts = textToSplit.Split('|');                
+                parts = parts.Skip(1).ToArray();               
+                Console.WriteLine(string.Join(" ", parts));
+            }
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("El archivo no se pudo leer: " + e.Message);
+        }
+    }
+}
